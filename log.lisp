33d3386954f925b9da03fb7e2bc4f1077cf1d4bf
Frank James <frank.a.james@gmail.com>
2015-03-28 14:18:16 +0000

    new plog API

--- log.lisp
+++ log.lisp
@@ -3,15 +3,17 @@
 
 (in-package #:frpc)
 
+(defparameter *frpc-log-path* (merge-pathnames (user-homedir-pathname) "frpc.log"))
 (defvar *frpc-log* nil)
 
 (defun frpc-log (lvl control-string &rest args)
   "Write a messagee to the debug log"
   (unless *frpc-log*
-    (let ((path (format nil "~A" (merge-pathnames (user-homedir-pathname) "frpc.log"))))
+    (let ((path (format nil "~A" *frpc-log-path*)))
       #+(or windows win32)(setf path (substitute #\\ #\/ path))
       (setf *frpc-log*
 	    (pounds.log:open-log :path path
 				 :tag "FRPC"))))
-  (apply #'pounds.log:write-message *frpc-log* lvl control-string argS))
+  (pounds.log:write-message *frpc-log* lvl 
+			    (apply #'format nil control-string args)))
 
